9339ab8534b02ad644cf8be5c2150e5b34ce97a4
Michael Stephens <michael@stephens.tech>
2022-08-01 18:10:03 -0700

    Upgraded for optional dx9 or dx11 install

--- Gw2ArcdpsLauncher/Program.cs
+++ Gw2ArcdpsLauncher/Program.cs
@@ -6,13 +6,66 @@ namespace Gw2ArcdpsLauncher
     {
         static void Main(string[] args)
         {
-            string arcdpsSource = $@"C:\Users\{Environment.UserName}\Downloads\d3d9.dll";
+            Console.WriteLine("Select which version of Arcdps:");
+            Console.WriteLine("[1] DX9");
+            Console.WriteLine("[2] DX11");
+            string arcDpsVersion = Console.ReadLine();
+
+            string arcDpsFileName = "";
+            string arcDpsInstallDir = "";
+            switch (arcDpsVersion)
+            {
+                case "1":
+                    arcDpsFileName = "d3d9.dll";
+                    arcDpsInstallDir = @"\bin64\d3d9.dll";
+                    break;
+                case "2":
+                    arcDpsFileName = "d3d11.dll";
+                    arcDpsInstallDir = @"\d3d11.dll";
+                    break;
+                default:
+                    Console.WriteLine("Must select a valid option.");
+                    return;
+            }
+
+                    Console.Write("Detecting Downloads directory...");
+            string arcdpsSource = "";
+
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in allDrives)
+            {
+                string potentialDownloadDir = d.Name + @"Users\" + Environment.UserName + @"\Downloads\";
+                string potentialDownloadDir2 = d.Name + @"Downloads\";
+                if (Directory.Exists(potentialDownloadDir))
+                {
+                    arcdpsSource = potentialDownloadDir + arcDpsFileName;
+                }
+                else if (Directory.Exists(potentialDownloadDir2))
+                {
+                    arcdpsSource = potentialDownloadDir2 + arcDpsFileName;
+                }
+            }
+
+            if (arcdpsSource == "")
+            {
+                Console.WriteLine("Failed!\n");
+                Console.WriteLine("Could not locate Downloads directory");
+                Console.WriteLine("    You may have an unusual Downloads directory, currently only Downloads in DRIVE:\\User\\USERNAME\\Downloads or DRIVE:\\Downloads are supported.");
+                Console.WriteLine("    Please either open an issue on Github with details of your installation or use a normal Downloads setup.\n");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Done!");
+                Console.WriteLine($"Downloads directory detected at {arcdpsSource}!\n");
+            }
 
             // Detect Gw2 install
             Console.Write("Detecting GW2 install directory...");
             string gw2Directory = "";
 
-            DriveInfo[] allDrives = DriveInfo.GetDrives();
             foreach (DriveInfo d in allDrives)
             {
                 string potentialGw2InstallDirectory = d.Name + @"\Program Files\Guild Wars 2\";
@@ -48,7 +101,7 @@ namespace Gw2ArcdpsLauncher
 
             // Copy to main Gw2 directory
             Console.Write($"Copying to GW2 install at {gw2Directory}...");
-            File.Copy(arcdpsSource, gw2Directory + @"\bin64\d3d9.dll", true);
+            File.Copy(arcdpsSource, $"{gw2Directory}{arcDpsInstallDir}", true);
             Console.WriteLine("Done!");
 
             // Check if GW2 Launcher exists, if so update Arcdps
@@ -58,7 +111,7 @@ namespace Gw2ArcdpsLauncher
                 Console.WriteLine("\nGw2Launcher detected... Copying Arcdps");
                 foreach (string folderName in Directory.GetDirectories(gw2LauncherDirectory))
                 {
-                    string fullPath = folderName + @"\bin64\d3d9.dll";
+                    string fullPath = $"{folderName}{arcDpsInstallDir}";
                     Console.Write($"Copying to {fullPath}...");
                     File.Copy(arcdpsSource, fullPath, true);
                     Console.WriteLine("Done!");
